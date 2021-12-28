9fa4e229d20e6db37c4f4c0dd32111c279497d47
Ratthapit <pimpitchakanitpanyajaroen@gmail.com>
2021-12-28 16:35:41 +0700

    lab14_1.cpp completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,35 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+
+    double sum,sum_pow2,sum2,sum3;
+    for(int i=0;i<N;i++){
+        sum 		+= A[i];
+        sum_pow2 	+= pow(A[i],2);
+        sum2 		+= log10(A[i]); 
+        sum3 		+= 1/A[i];
+    }
+    
+    double c[N];
+    for(int i=0;i<N;i++)		c[i]=A[i];
+    
+    double x;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N-1;j++){
+            if(c[j]>c[j+1]){
+                x		=c[j];
+                c[j]	=c[j+1];
+                c[j+1]	=x;
+            }
+        }
+    }
+
+    B[0]=sum/N;
+    B[1]=sqrt((sum_pow2/N)-pow((sum/N),2));
+    B[2]=pow(10,(sum2/N));
+    B[3]=N/sum3;
+    B[4]=c[N-1];
+    B[5]=c[0];
+}
